58ce93731670ac4bf5bda1553bdd333ca1c2489b
CapybaraYT1 <Capybarayt2.official@gmail.com>
2024-08-29 10:49:41 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function openn(){
+    document.querySelector("button").style.display="none"
+    document.querySelector("#model_into").style.display="flex"
+}
+function closse(){
+    document.querySelector("button").style.display="block"
+    document.querySelector("#model_into").style.display="none"
+}
